ddccb081e927c5589e496d564cafc4b4cd23565b
Jordy Pena <jordypenamontes@gmail.com>
2020-11-02 12:39:49 -0600

    first commit

---
+++ src/practice.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+})
+
+const qry = knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where({ name: 'Point of view gun' })
+  .first()
+  .toQuery()
+  //.then(result => {
+    //console.log(result)
+  //})
+
+  console.log(qry)
+
+  const searchTerm = 'holo'
+
+knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where('name', 'ILIKE', `%${searchTerm}%`)
+  .then(result => {
+    console.log(result)
+  })
+
+  function searchByProduceName(searchTerm) {
+    knexInstance
+      .select('product_id', 'name', 'price', 'category')
+      .from('amazong_products')
+      .where('name', 'ILIKE', `%${searchTerm}%`)
+      .then(result => {
+        console.log(result)
+      })
+  }
+  
+  searchByProduceName('holo')
